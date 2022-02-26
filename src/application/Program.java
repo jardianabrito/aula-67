6efe01e7489a1d6edeb4d1b36f7862bff977a5ea
jardianabrito <rodrigoclickaki@gmail.com>
2022-02-26 20:18:05 -0300

    Implementando construtor

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,23 +11,23 @@ public class Program {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-
-		Product product = new Product();
 		
 		System.out.println("Enter product data:");
 		System.out.print("Name: ");
-		product.name = sc.next();
+		String name = sc.next();
 		System.out.print("Price: ");
-		product.price = sc.nextDouble();
+		double price = sc.nextDouble();
 		System.out.print("Quantity in stock: ");
-		product.quantity = sc.nextInt();
+		int quantity = sc.nextInt();
+		
+		Product product = new Product(name, price, quantity);
 		
 		System.out.println();
 		System.out.println("Product data: " + product.toString());
 		
 		System.out.println();
 		System.out.print("Enter the number of products to be added in stock: ");
-		int quantity = sc.nextInt();		
+		quantity = sc.nextInt();		
 		product.addProduct(quantity);
 		System.out.println();
 		System.out.println("Updated data: " + product.toString());
